c0a8937903a6cf1830d419cdf7f14b74725945fc
Eni <impossibleorganism@gmail.com>
2024-09-25 15:38:31 -0500

    Ran cargo fmt.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -3,13 +3,16 @@ extern crate uom;
 
 use uom::fmt::DisplayStyle::Abbreviation;
 
-use crate::units::quantities::Mass;
 use crate::units::mass;
+use crate::units::quantities::Mass;
 
 mod elements;
 mod units;
 
 pub fn read(string: String) {
     let amount: Mass = string.parse::<Mass>().unwrap();
-    println!("'{string}' is {}.", amount.into_format_args(mass::kilogram, Abbreviation));
+    println!(
+        "'{string}' is {}.",
+        amount.into_format_args(mass::kilogram, Abbreviation)
+    );
 }
