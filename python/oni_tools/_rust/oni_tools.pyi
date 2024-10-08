923387772d2f6aa1e9a663bb8224d543fcc7bc41
Eni <impossibleorganism@gmail.com>
2024-10-08 14:30:31 -0500

    Added example shell for geyser rate function.

--- python/oni_tools/_rust/oni_tools.pyi
+++ python/oni_tools/_rust/oni_tools.pyi
@@ -2,4 +2,13 @@
 Python interface for the oni-tools crate.
 """
 
+def geyser_rate(
+    geyser_type: str | float,
+    eruption_rate: str | float,
+    eruption_duration: str | float,
+    eruption_period: str | float,
+    active_duration: str | float,
+    active_period: str | float,
+) -> str | float: ...
+
 def sum_as_string(a: int, b: int) -> str: ...
