ae52dea7bd431bea2e261a3169f42fe7b44258f8
Eni <impossibleorganism@gmail.com>
2024-10-26 14:22:03 -0500

    Added notebooks to the gitignore, a demo_tools package for pure-Python code, and __version__ to packages.

--- python/oni_tools/__init__.py
+++ python/oni_tools/__init__.py
@@ -1,6 +1,10 @@
+import importlib.metadata
+
 from oni_tools._rust.oni_tools import (
     sum_as_string,
 )
+
+__version__ = importlib.metadata.version("oni-tools")
 
 __all__ = [
     "sum_as_string",
