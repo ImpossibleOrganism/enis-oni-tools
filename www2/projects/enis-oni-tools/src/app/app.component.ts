76f867ee0a7a16b7d3b8a4bd06437fdb0f94bd9a
Eni <impossibleorganism@gmail.com>
2025-01-19 16:55:03 -0600

    Added buttons to the Angular app so I can test.

--- www2/projects/enis-oni-tools/src/app/app.component.ts
+++ www2/projects/enis-oni-tools/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import * as oni_tools from 'oni-tools';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'enis-oni-tools';
+
+  // async ngOnInit() {
+  //  // Initialize the WASM module when the app loads
+  //  await oni_tools.init();
+  // }
+
+  onButtonOne() {
+    // Do a regular JS alert, to test.
+    alert('Button one has been clicked!');
+  }
+
+  onButtonTwo() {
+    // Call the greet function from WASM
+    alert('Button two has been clicked!');
+    // oni_tools.greet();
+  }
 }
