918246ad527fb06f3be12921fc7e6dd12b3bc3f0
Eni <impossibleorganism@gmail.com>
2025-01-20 16:15:36 -0600

    Trying to get Angular to work; adjusted project directory in package.json.

--- javascript/oni-tools.js
+++ javascript/oni-tools.js
@@ -1,1 +1,11 @@
 export { greet } from "./generated/oni_tools.js";
+
+// import init from "./generated/oni_tools_bg.wasm?init"
+//
+// init().then((instance) => {
+//     instance.exports.test()
+// })
+
+// export function greet() {
+//     alert("this is my dummy greeting")
+// }
